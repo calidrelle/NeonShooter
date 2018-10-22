bba9fa69ce55aeb7298ae5c83e9bdf8629d2c27a
Stephane Stephanos <stephane.stephanos@gmail.com>
2018-10-23 00:06:43 +0200

    Part 1

--- NeonShooter/GameRoot.cs
+++ NeonShooter/GameRoot.cs
@@ -1,27 +1,38 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace NeonShooter {
 
-    public class Game1 : Game {
+    public class GameRoot : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1() {
+        public static GameRoot Instance { get; private set; }
+        public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+        public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+
+        public GameRoot() {
+            Instance = this;
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            //graphics.IsFullScreen = true;
+            graphics.ApplyChanges();
         }
 
         protected override void Initialize() {
-
             base.Initialize();
+            EntityManager.Add(PlayerShip.Instance);
         }
 
         protected override void LoadContent() {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            Art.Load(Content);
         }
 
         protected override void UnloadContent() {
@@ -29,18 +40,25 @@ namespace NeonShooter {
         }
 
         protected override void Update(GameTime gameTime) {
+            Input.Update();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            EntityManager.Update();
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin(SpriteSortMode.Texture, BlendState.Additive);
+            EntityManager.Draw(spriteBatch);
+
+            // draw the custom mouse cursor
+            spriteBatch.Draw(Art.Pointer, Input.MousePosition, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
